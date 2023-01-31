6e38ffcb00b8daab5fad38500b309fd9e6cfe520
Samuel Bourassa <district67@gmail.com>
2023-01-31 01:53:16 -0500

    Add Tailwind and @tailwindcss/typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
