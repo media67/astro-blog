ad6816b29a13b8535eedde024b986ff678c8b482
Samuel Bourassa <district67@gmail.com>
2023-01-31 05:59:48 -0500

    Update UI with better styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				// Simple 8 row grid
+				'8': 'repeat(8, minmax(0, 1fr))',
+		
+				// Complex site-specific row configuration
+				'layout': 'auto 1fr auto',
+			  }
+		},
 	},
 	plugins: [
-		require('@tailwindcss/typography')
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms')
 	],
 }
